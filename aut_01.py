c1ce2acfb42ac0e9f2dfc102f8ad2dbf74f478e4
sysbot <systecinfo_cl@criptext.com>
2022-03-01 23:07:40 -0300

    test01

--- aut_01.py
+++ aut_01.py
@@ -11,7 +11,7 @@ def to_gb(bytes):
     "Convierte bytes a gigabytes."
     return bytes / 1024**3
 
-print("Espacio total: {:.2f} GB.".format(to_gb(disk_usage.total)))
-print("Espacio libre: {:.2f} GB.".format(to_gb(disk_usage.free)))
-print("Espacio usado: {:.2f} GB.".format(to_gb(disk_usage.used)))
-print("Porcentaje de espacio usado: {}%.".format(disk_usage.percent))
+print("Espacio total: {:.2f} GB.".format(to_gb(uso_disco.total)))
+print("Espacio libre: {:.2f} GB.".format(to_gb(uso_disco.free)))
+print("Espacio usado: {:.2f} GB.".format(to_gb(uso_disco.used)))
+print("Porcentaje de espacio usado: {}%.".format(uso_disco.percent))
