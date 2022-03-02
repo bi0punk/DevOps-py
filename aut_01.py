3ff20e4d64af3f8d36f3c107f513e1b6bcd64bed
sysbot <systecinfo_cl@criptext.com>
2022-03-01 23:12:53 -0300

    test01

--- aut_01.py
+++ aut_01.py
@@ -15,3 +15,9 @@ print("Espacio total: {:.2f} GB.".format(transforma_gb(uso_disco.total)))
 print("Espacio libre: {:.2f} GB.".format(transforma_gb(uso_disco.free)))
 print("Espacio usado: {:.2f} GB.".format(transforma_gb(uso_disco.used)))
 print("Porcentaje de espacio usado: {}%.".format(uso_disco.percent))
+
+
+if uso_disco.free >= 37.15:
+    print("ALERTA POCO ESPACIO")
+else:
+    print("NO HAY PROBLEMA, ESPACIO DISPONIBLE")
