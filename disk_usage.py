b145d630b1ff7dbc4c35badb0a91407b4fd7a03e
sysbot <systecinfo_cl@criptext.com>
2022-03-06 22:39:07 -0300

    test04

--- disk_usage.py
+++ disk_usage.py
@@ -3,6 +3,7 @@ from humanize import naturalsize
 
 def var_size():
     size = os.stat('/var/log/').st_size
+    print("El tamaño del directorio es: " +str(size))
     print(size)
     print(naturalsize(size))
     print(type(naturalsize(size)))
