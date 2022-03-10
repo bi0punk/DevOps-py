aed22562c504e90b92c085f6cfcaabce2f181954
Enrique Suarez <enrique.suarez@kyndryl.com>
2022-03-10 19:02:20 -0300

    test

---
+++ ansible_auto.py
@@ -0,0 +1,13 @@
+from tqdm import tqdm
+import python_console_menu
+
+
+print("_________________________________________")
+print("| ######## ANSIBLE AUTOMATION TASK ######|")
+print("-----------------------------------------")
+
+
+
+
+for i in tqdm (range (100), desc="Loading..."):
+    pass
