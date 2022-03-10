4ad7c27f34056f90f2ba090aba381ad285bdf360
Enrique Suarez <enrique.suarez@kyndryl.com>
2022-03-10 20:41:24 -0300

    test

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-def pedirNumeroEntero():
- 
+
+def menuA():
     correcto=False
     num=0
     while(not correcto):
@@ -7,10 +7,8 @@ def pedirNumeroEntero():
             num = int(input("Introduce un numero entero: "))
             correcto=True
         except ValueError:
-            print('Error, introduce un numero entero')
-     
+            print('Error, introduce un numero entero')  
     return num
- 
 salir = False
 opcion = 0
  
@@ -21,24 +19,27 @@ while not salir:
     print("        +-+-+-+-+")
     print("CONSOLE ANSIBLE AUTOMATION PACK\n")
     
-    print ("1. Realizar Ping")
-    print ("2. Instalación Mariadb")
-    print ("3. Eliminación de Logs")
-    print ("4. Salir")
-     
-    print ("Elige una opcion")
- 
-    opcion = pedirNumeroEntero()
+    print("1.- ---- Realizar Ping ----")
+    print("2.- ---- Instalación Mariadb ----")
+    print("3.- ---- Eliminación de Logs ----")
+    print("4.- ---- Listar Máquinas ----")
+    print("5.-       * Salir *")
+      
+    opcion = menuA()
  
     if opcion == 1:
-        print ("Opcion 1")
+        def ping():
+            print ("Opcion 1")
+
+            
     elif opcion == 2:
         print ("Opcion 2")
     elif opcion == 3:
         print("Opcion 3")
     elif opcion == 4:
+        print("Opcion 3\n")
+    elif opcion == 5:
         salir = True
     else:
-        print ("Introduce un numero entre 1 y 3")
+        print ("\nIntroduce un numero entre 1 y 5")
  
-print ("Fin")
